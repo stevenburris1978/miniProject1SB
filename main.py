b17bf1dfd6ceda99c7b3c6c4c119bf1cd4f104d2
stevenburris1978 <s_burris@mail.fhsu.edu>
2023-09-11 13:21:56 -0500

    added five stock tickers

--- main.py
+++ main.py
@@ -6,15 +6,59 @@ import yfinance as yf
 
 msft = yf.Ticker("MSFT")
 
-data = yf.download("PZZA", start="2023-08-17", end="2023-08-31")
+dataOne = yf.download("PZZA", start="2023-08-17", end="2023-08-31")
 
-print(data['Close'])
+print(dataOne['Close'])
 
-z = (data['Close'])
+z = (dataOne['Close'])
 
 y = np.array(z)
 
 plt.plot(y, 'red')
+plt.show()
+
+dataTwo = yf.download("AAPL", start="2023-08-17", end="2023-08-31")
+
+print(dataTwo['Close'])
+
+u = (dataTwo['Close'])
+
+w = np.array(u)
+
+plt.plot(w, 'red')
+plt.show()
+
+dataThree = yf.download("LUV", start="2023-08-17", end="2023-08-31")
+
+print(dataThree['Close'])
+
+t = (dataThree['Close'])
+
+v = np.array(t)
+
+plt.plot(v, 'red')
+plt.show()
+
+dataFour = yf.download("TSLA", start="2023-08-17", end="2023-08-31")
+
+print(dataFour['Close'])
+
+r = (dataFour['Close'])
+
+s = np.array(r)
+
+plt.plot(s, 'red')
+plt.show()
+
+dataFive = yf.download("AMZN", start="2023-08-17", end="2023-08-31")
+
+print(dataFive['Close'])
+
+o = (dataFive['Close'])
+
+p = np.array(o)
+
+plt.plot(p, 'red')
 plt.show()
 
 b = [2, 1, 5, 7, 4, 6, 8, 14, 10, 9, 18]
